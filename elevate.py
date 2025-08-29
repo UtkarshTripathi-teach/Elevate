3a7651613e1e33e120a778a71f10b986db626e01
Utkarsh Tripathi <tripathiutkarsh.tech@outlook.com>
2025-08-30 03:11:24 +0530

    Update elevate.py

--- elevate.py
+++ elevate.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
 import os
+import random
 
 from data_manager import DataManager
 from ml_analyzer import MLAnalyzer
